eb347f640a8618b6f9688037b6e2729125c4fb83
Fynn Frings <fynn.frings@gmail.com>
2024-04-24 11:12:58 +0200

    email fix

--- src/app/api/send-mail/route.js
+++ src/app/api/send-mail/route.js
@@ -3,42 +3,50 @@ import { NextResponse } from "next/server";
 
 // Setting an async function to send a message via nodemailer
 export async function POST(req) {
-	// setting a body of message
-	const data = await req.json();
+  // setting a body of message
+  const data = await req.json();
 
-	const message = {
-		to: `${data.email}`,
-		from: process.env.SMTP_USER,
-		subject: `Message from ${data.email}. ${data.firstname} ${data.lastname}`,
-		text: data.message,
-	};
+  const message = {
+    to: `${data.email}`,
+    from: process.env.SMTP_USER,
+    subject: `Bestätigung Anfrage Clubbery Kontaktformular`,
+    text: `Vielen Dank für deine Nachricht ${data.firstname}, wir freuen uns von dir zu hören! \n\nUnser Support-Team wird sich schnellstmöglich mit dir in Verbindung setzen.\n\n${data.message}\n\nMit freundlichen Grüßen\nDein Clubbery Support\n\n So lautet die Nachricht, welche wir von dir erhalten haben:`,
+  };
 
-	// setting a properties for nodemailer
-	const transporter = nodemailer.createTransport({
-		host: String(process.env.SMTP_HOST),
-		port: Number(process.env.SMTP_PORT),
-		auth: {
-			user: process.env.SMTP_USER,
-			pass: process.env.SMTP_PASSWORD,
-		},
-	});
+  const supportMessage = {
+    to: `support@clubbery.com`,
+    from: process.env.SMTP_USER,
+    subject: `Neue Anfrage von ${data.firstname} ${data.lastname}`,
+    text: `Titel: Anfrage Clubbery Kontaktformular\n\nKundendaten: ${data.email}, ${data.firstname} ${data.lastname}\n\nNachricht: ${data.message}}`,
+  };
 
-	// Condition to check content of message
+  // setting a properties for nodemailer
+  const transporter = nodemailer.createTransport({
+    host: String(process.env.SMTP_HOST),
+    port: Number(process.env.SMTP_PORT),
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-	if (req.method === "POST") {
-		if (!data.email || !data.message || !data.firstname) {
-			return NextResponse.json({ message: "Bad request!" }, { status: 400 });
-		}
-		// defining an async function to send a message
-		try {
-			await transporter.sendMail(message);
-			return NextResponse.json({ success: true }, { status: 200 });
-			//gandling an error
-		} catch (error) {
-			console.log(error);
-			return NextResponse.json({ message: "Bad request!" }, { status: 400 });
-		}
-	}
-	// Returning a http status
-	return NextResponse.json({ message: "Bad request!" }, { status: 400 });
+  // Condition to check content of message
+
+  if (req.method === "POST") {
+    if (!data.email || !data.message || !data.firstname) {
+      return NextResponse.json({ message: "Bad request!" }, { status: 400 });
+    }
+    // defining an async function to send a message
+    try {
+      await transporter.sendMail(message);
+      await transporter.sendMail(supportMessage);
+      return NextResponse.json({ success: true }, { status: 200 });
+      //gandling an error
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Bad request!" }, { status: 400 });
+    }
+  }
+  // Returning a http status
+  return NextResponse.json({ message: "Bad request!" }, { status: 400 });
 }
