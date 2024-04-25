cb3d659a02b248424fe457b44c0481d653e77272
Fynn Frings <fynn.frings@gmail.com>
2024-04-25 10:06:05 +0200

    new images

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -17,7 +17,7 @@ const Landing = () => {
 				<div className="-z-10 absolute top-24 right-8 lg:top-36 lg:right-6 xl:top-44 xl:right-8 w-24 h-24 lg:w-36 lg:h-36 bg-orange-400 rounded-full blur-3xl"></div>
 				<div className="-z-10 absolute bottom-24 left-24 lg:bottom-44 lg:left-32 xl:bottom-48 xl:left-32 w-24 h-24 lg:w-36 lg:h-36 bg-yellow-500 rounded-full blur-3xl"></div>
 				<div className="-z-10 absolute -bottom-5 right-14 lg:-bottom-5 lg:right-28 xl:-bottom-5 xl:right-32 w-24 h-24 lg:w-36 lg:h-36 bg-sky-500 rounded-full blur-3xl"></div>
-				<Image className="z-10" src="/iphonemockupclubbery.png" alt="phone" width={900} height={900} />
+				<Image className="z-10" src="/iphonemockupsclubbery.png" alt="phone" width={1200} height={1500} />
 			</div>
 		</div>
 	);
