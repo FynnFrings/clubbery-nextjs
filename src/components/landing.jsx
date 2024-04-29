87b210231d386b263cee8f350dc839909b88baed
Fynn Frings <69033928+FynnFrings@users.noreply.github.com>
2024-04-29 19:29:18 +0000

    changing link

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,26 +2,52 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Landing = () => {
-	return (
-		<div className="flex flex-col md:flex-row md:justify-evenly md:items-center text-white  w-full">
-			<div className="flex flex-col gap-y-5 w-full md:w-2/3">
-				<h1 className="text-3xl md:text-5xl font-bold">Dein Ticket in der Hosentasche</h1>
-				<p className="text-lg">Entdecke die Welt der Partys mit Clubbery: Kaufe Tickets oder präsentiere deine eigenen Partys als Veranstalter. Tauche ein in eine Welt voller Möglichkeiten und erlebe unvergessliche Momente mit uns!</p>
-				<h2>Bald hier verfügbar:</h2>
-				<div className="flex flex-col md:flex-row gap-5">
-					<Image src="/badges/apple-badge.svg" alt="Laden in App Store" width={150} height={50} />
-					<Image src="/badges/google-play-badge.png" alt="Laden in Google Play Store" width={150} height={50} />
-				</div>
-			</div>
-			<div className="hidden md:block relative">
-				<div className="-z-10 absolute top-6 left-36 lg:top-6 lg:left-36 xl:top-6 xl:left-48 w-24 h-24 lg:w-36 lg:h-36 bg-violet-500 rounded-full blur-3xl"></div>
-				<div className="-z-10 absolute top-24 right-8 lg:top-36 lg:right-6 xl:top-44 xl:right-8 w-24 h-24 lg:w-36 lg:h-36 bg-orange-400 rounded-full blur-3xl"></div>
-				<div className="-z-10 absolute bottom-24 left-24 lg:bottom-44 lg:left-32 xl:bottom-48 xl:left-32 w-24 h-24 lg:w-36 lg:h-36 bg-yellow-500 rounded-full blur-3xl"></div>
-				<div className="-z-10 absolute -bottom-5 right-14 lg:-bottom-5 lg:right-28 xl:-bottom-5 xl:right-32 w-24 h-24 lg:w-36 lg:h-36 bg-sky-500 rounded-full blur-3xl"></div>
-				<Image className="z-10" src="/iphonemockupsclubbery.png" alt="phone" width={1200} height={1500} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col md:flex-row md:justify-evenly md:items-center text-white  w-full">
+      <div className="flex flex-col gap-y-5 w-full md:w-2/3">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Dein Ticket in der Hosentasche
+        </h1>
+        <p className="text-lg">
+          Entdecke die Welt der Partys mit Clubbery: Kaufe Tickets oder
+          präsentiere deine eigenen Partys als Veranstalter. Tauche ein in eine
+          Welt voller Möglichkeiten und erlebe unvergessliche Momente mit uns!
+        </p>
+        <div className="flex flex-col md:flex-row gap-5">
+          <Link
+            target="_blank"
+            href="https://apps.apple.com/de/app/clubbery/id6476625439"
+          >
+            <Image
+              src="/badges/apple-badge.svg"
+              alt="Laden in App Store"
+              width={150}
+              height={50}
+            />{" "}
+          </Link>
+          <Image
+            src="/badges/google_play_store_black_and_white.png"
+            alt="Laden in Google Play Store"
+            width={150}
+            height={50}
+          />
+        </div>
+      </div>
+      <div className="hidden md:block relative">
+        <div className="-z-10 absolute top-6 left-36 lg:top-6 lg:left-36 xl:top-6 xl:left-48 w-24 h-24 lg:w-36 lg:h-36 bg-violet-500 rounded-full blur-3xl"></div>
+        <div className="-z-10 absolute top-24 right-8 lg:top-36 lg:right-6 xl:top-44 xl:right-8 w-24 h-24 lg:w-36 lg:h-36 bg-orange-400 rounded-full blur-3xl"></div>
+        <div className="-z-10 absolute bottom-24 left-24 lg:bottom-44 lg:left-32 xl:bottom-48 xl:left-32 w-24 h-24 lg:w-36 lg:h-36 bg-yellow-500 rounded-full blur-3xl"></div>
+        <div className="-z-10 absolute -bottom-5 right-14 lg:-bottom-5 lg:right-28 xl:-bottom-5 xl:right-32 w-24 h-24 lg:w-36 lg:h-36 bg-sky-500 rounded-full blur-3xl"></div>
+        <Image
+          className="z-10"
+          src="/iphonemockupsclubbery.png"
+          alt="phone"
+          width={1200}
+          height={1500}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -8,73 +8,96 @@ import { useState } from "react";
 import { useScrollPosition } from "@/app/hooks/useScrollPosition";
 
 const Header = () => {
-	const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
 
-	const handleOnClick = () => {
-		setMenu(!menu);
-	};
-	// const device = useMobileDetect();
-	const scrollPosition = useScrollPosition();
-	return (
-		<header className="sticky top-0 z-50 mb-8 lg:mb-5">
-			<div className={`${scrollPosition > 0 || menu ? "bg-[#262730]" : "bg-transparent"} ${scrollPosition > 0 && !menu ? "shadow-lg" : "shadow-none"} transition-colors ease-in duration-100 flex flex-row justify-between items-center w-full text-white py-4 px-5 md:px-10`}>
-				<Link href="/" className="hover_button_animation">
-					<Image src="/logo.png" alt="Clubbery Logo Image" width={45} height={50} />
-				</Link>
-				<ul className="hidden md:flex flex-row justify-between items-center list-none gap-x-5 lg:gap-x-12 text-base">
-					{/* <li>
+  const handleOnClick = () => {
+    setMenu(!menu);
+  };
+  // const device = useMobileDetect();
+  const scrollPosition = useScrollPosition();
+  return (
+    <header className="sticky top-0 z-50 mb-8 lg:mb-5">
+      <div
+        className={`${
+          scrollPosition > 0 || menu ? "bg-[#262730]" : "bg-transparent"
+        } ${
+          scrollPosition > 0 && !menu ? "shadow-lg" : "shadow-none"
+        } transition-colors ease-in duration-100 flex flex-row justify-between items-center w-full text-white py-4 px-5 md:px-10`}
+      >
+        <Link href="/" className="hover_button_animation">
+          <Image
+            src="/logo.png"
+            alt="Clubbery Logo Image"
+            width={45}
+            height={50}
+          />
+        </Link>
+        <ul className="hidden md:flex flex-row justify-between items-center list-none gap-x-5 lg:gap-x-12 text-base">
+          {/* <li>
 						<Link href="/not-found">
 							<p className="hover_text_animation">So funktioniert Clubbery</p>
 						</Link>
 					</li> */}
-					<li>
-						<Link href="/faq">
-							<p className="hover_text_animation">FAQ</p>
-						</Link>
-					</li>
-					<li>
-						<Link href="/contact">
-							<p className="hover_text_animation">Kontakt</p>
-						</Link>
-					</li>
-					<li className="hover_button_animation">
-						<Link href="/not-found" className="px-5 py-3 rounded-2xl bg-[#CC7503] text-[#F0FDF4]">
-							Download Clubbery
-						</Link>
-					</li>
-				</ul>
-				<div className="block md:hidden">
-					<button type="button" onClick={() => handleOnClick()}>
-						{menu ? <AiOutlineClose className="text-white w-7 h-7" /> : <RxHamburgerMenu className="text-white w-10 h-10 " />}
-					</button>
-				</div>
-			</div>
-			<div className={`absolute md:hidden ${menu ? "translate-y-0" : "-translate-y-[200%]"} bg-[#262730] w-full pb-5 transition ease-in-out shadow-lg -z-10`}>
-				<ul className="flex flex-col gap-y-3 items-center list-none gap-x-12 text-white text-lg">
-					{/* <li>
+          <li>
+            <Link href="/faq">
+              <p className="hover_text_animation">FAQ</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <p className="hover_text_animation">Kontakt</p>
+            </Link>
+          </li>
+          <li className="hover_button_animation">
+            <Link
+              target="_blank"
+              href="https://apps.apple.com/de/app/clubbery/id6476625439"
+              className="px-5 py-3 rounded-2xl bg-[#CC7503] text-[#F0FDF4]"
+            >
+              Download Clubbery
+            </Link>
+          </li>
+        </ul>
+        <div className="block md:hidden">
+          <button type="button" onClick={() => handleOnClick()}>
+            {menu ? (
+              <AiOutlineClose className="text-white w-7 h-7" />
+            ) : (
+              <RxHamburgerMenu className="text-white w-10 h-10 " />
+            )}
+          </button>
+        </div>
+      </div>
+      <div
+        className={`absolute md:hidden ${
+          menu ? "translate-y-0" : "-translate-y-[200%]"
+        } bg-[#262730] w-full pb-5 transition ease-in-out shadow-lg -z-10`}
+      >
+        <ul className="flex flex-col gap-y-3 items-center list-none gap-x-12 text-white text-lg">
+          {/* <li>
 						<Link onClick={() => handleOnClick()} href="/not-found">
 							<p className="hover_text_animation">So funktioniert Clubbery</p>
 						</Link>
 					</li> */}
-					<li>
-						<Link onClick={() => handleOnClick()} href="/faq">
-							<p className="hover_text_animation">FAQ</p>
-						</Link>
-					</li>
-					<li>
-						<Link onClick={() => handleOnClick()} href="/contact">
-							<p className="hover_text_animation">Kontakt</p>
-						</Link>
-					</li>
-					<li className="px-5 py-3 rounded-2xl bg-[#CC7503] text-[#F0FDF4] hover_button_animation">
-						<Link onClick={() => handleOnClick()} href="/not-found">
-							Download Clubbery
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</header>
-	);
+          <li>
+            <Link onClick={() => handleOnClick()} href="/faq">
+              <p className="hover_text_animation">FAQ</p>
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => handleOnClick()} href="/contact">
+              <p className="hover_text_animation">Kontakt</p>
+            </Link>
+          </li>
+          <li className="px-5 py-3 rounded-2xl bg-[#CC7503] text-[#F0FDF4] hover_button_animation">
+            <Link onClick={() => handleOnClick()} href="/not-found">
+              Download Clubbery
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
