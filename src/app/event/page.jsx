9ed4fc65b44d78e5e73e7d6322074ca190d99f6d
Fynn Frings <69033928+FynnFrings@users.noreply.github.com>
2024-05-15 08:58:47 +0000

    event page

---
+++ src/app/event/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+function EventPage() {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center gap-5">
+            <p className="text-white text-3xl font-semibold">Installiere die Clubbery App um, das Event zu sehen!</p>
+            <div className="flex flex-col md:flex-row gap-5">
+                <Link
+                    target="_blank"
+                    href="https://apps.apple.com/de/app/clubbery/id6476625439"
+                >
+                    <Image
+                        src="/badges/apple-badge.png"
+                        alt="Laden in App Store"
+                        width={150}
+                        height={50}
+                    />{" "}
+                </Link>
+                <Link
+                    target="_blank"
+                    href="https://play.google.com/store/apps/details?id=net.swibble.clubberyApp"
+                >
+                    <Image
+                        src="/badges/google-play-badge.png"
+                        alt="Laden in Google Play Store"
+                        width={150}
+                        height={50}
+                    />
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default EventPage;
