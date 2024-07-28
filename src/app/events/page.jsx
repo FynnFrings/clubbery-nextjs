61b030609f098dd0b30b392e8f844d3074731281
Bukabuka321 <20uva02@gmail.com>
2024-07-28 21:35:29 +0200

    events page basic overview

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 import ComingSoonBanner from "@/components/ComingSoonBanner";
+import ErrorComponent from "@/components/ErrorComponent";
 import EventCard from "@/components/Event/EventCard";
 import SearchField from "@/components/InputForms/SearchField";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import useSWR from "swr";
@@ -10,8 +12,6 @@ function EventPage() {
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 	const { data, isLoading, error } = useSWR("https://getallevents-qh42lmu4jq-uc.a.run.app", fetcher);
-	console.log("🚀 ~ EventPage ~ data:", data);
-	console.log("🚀 ~ EventPage ~ error:", error);
 
 	const [eventsSearchInput, setEventsSearchInput] = useState("");
 
@@ -32,22 +32,24 @@ function EventPage() {
 			}, 4000);
 	}, [showBanner]);
 
-	// if (isLoading) return <div>Loading</div>;
+	if (isLoading) return <LoadingSpinner />;
 
-	// if (error) return <div>Error</div>;
+	if (error) return <ErrorComponent />;
 
 	return (
 		<>
 			<div className="flex flex-col gap-y-5">
 				<div className="w-full h-32 md:h-56 relative">
-					<Image src="/party.webp" alt="Events" width={3300} height={2200} className="bg-cover object-cover opacity-60 bg-center rounded-xl w-full h-full" />
+					<Image priority src="/party.webp" alt="Events" width={3300} height={2200} className="bg-cover object-cover opacity-60 bg-center rounded-xl w-full h-full" />
 					<h1 className="text-white text-5xl absolute top-1/2 left-1/2 -translate-x-1/2">Events</h1>
 				</div>
 				<div>
 					<SearchField handleSearchInputChange={handleInputSearchChange} searchInput={eventsSearchInput} searchIputPlaceholder={"Search"} />
 				</div>
 				<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8 ">
-					<EventCard handleShowBanner={handleShowBanner} />
+					{data.map((eventData) => (
+						<EventCard eventData={eventData} key={eventData.id} handleShowBanner={handleShowBanner} />
+					))}
 				</div>
 			</div>
 			{showBanner && <ComingSoonBanner />}
