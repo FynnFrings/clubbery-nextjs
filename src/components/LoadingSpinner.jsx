96dd573495055661ef7c41f7473b8e883c41f400
Bukabuka321 <20uva02@gmail.com>
2024-07-28 21:34:49 +0200

    created loading spinner component

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const LoadingSpinner = () => {
+	return (
+		<div className="w-full h-screen gap-y-16 flex flex-col items-center justify-center">
+			<Image src="/logo.png" alt="loading_image" width={90} height={90} />
+			<div className="h-3 w-3/4 md:w-1/3 bg-transparent rounded-lg overflow-hidden">
+				<div className="h-full w-1/2 bg-clubbery-orange animate-loader rounded-lg"></div>
+			</div>
+		</div>
+	);
+};
+
+export default LoadingSpinner;
