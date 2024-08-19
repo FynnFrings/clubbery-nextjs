2066b081e70dcf9aa86f530b89f743e90b78fcc0
Bukabuka321 <20uva02@gmail.com>
2024-08-19 20:18:13 +0200

    added env variables

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCE7APuoKrs24idf23aX1Dj5lcx9oMGGZg",
-	authDomain: "clubbery-dev.firebaseapp.com",
-	projectId: "clubbery-dev",
-	storageBucket: "clubbery-dev.appspot.com",
-	messagingSenderId: "401395826376",
-	appId: "1:401395826376:web:54e8e9050fe2b3e4f86ffd",
-	measurementId: "G-R49PZVPWSK",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
