09ed5a487cab680605c833fbcf4075458349d375
Fynn Frings <69033928+FynnFrings@users.noreply.github.com>
2024-08-28 16:01:46 +0200

    Update page.jsx

--- src/app/events/[slug]/page.jsx
+++ src/app/events/[slug]/page.jsx
@@ -212,9 +212,7 @@ const EventDetailsPage = ({ params }) => {
 	return (
 		<>
 			<div className=" py-8 grid gap-8 text-white border-b border-gray-700 md:grid-cols-2 md:grid-rows-auto md:gap-4 md:pt-0">
-				<div className="flex justify-center items-center  md:col-span-2 ">
-					<Image className=" aspect-video object-cover rounded-lg " src={eventImage.url} alt={eventImage.path} width={1400} height={275} priority />
-				</div>
+				
 				<div>
 					<h1 className="text-2xl">{eventDetails.title}</h1>
 					<p>{eventInfo.categoryName}</p>
