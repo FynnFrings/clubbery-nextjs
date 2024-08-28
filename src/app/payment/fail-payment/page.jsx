80cac8f37302b0035256ba78c3c77d05f3a8305f
Fynn Frings <69033928+FynnFrings@users.noreply.github.com>
2024-08-28 15:37:11 +0200

    Update page.jsx

--- src/app/payment/fail-payment/page.jsx
+++ src/app/payment/fail-payment/page.jsx
@@ -7,7 +7,7 @@ const FailPayment = () => {
 				<div className="bg-[#3e3f4ae8] shadow-xl text-white p-10 items-center flex flex-col gap-5">
 					<BiSolidError className="text-red-500 h-24 w-24" />
 					<h1 className="text-3xl">Fehler!</h1>
-					<p className="text-xl w-3/5 text-center">Es gab ein Problem mit deiner Bestellung! Du kannst diese Website jetzt schließen.</p>
+					<p className="text-xl w-3/5 text-center">Es gab ein Problem mit deinem Auftrag! Du kannst diese Website jetzt schließen.</p>
 				</div>
 			</div>
 		</>
