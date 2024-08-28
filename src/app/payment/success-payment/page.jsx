128ebfe7cf84fce6097abdb3f09ef162d84a8539
Fynn Frings <69033928+FynnFrings@users.noreply.github.com>
2024-08-28 15:36:51 +0200

    Update page.jsx

--- src/app/payment/success-payment/page.jsx
+++ src/app/payment/success-payment/page.jsx
@@ -7,7 +7,7 @@ const SuccessfulPayment = () => {
 				<div className="bg-[#3e3f4ae8] shadow-xl text-white p-10 items-center flex flex-col gap-5">
 					<IoMdCheckmarkCircle className="text-[#CC7503] h-24 w-24" />
 					<h1 className="text-3xl">Erfolgreich!</h1>
-					<p className="text-xl w-3/5 text-center">Wir haben deine Bestellung erhalten! Du kannst diese Website jetzt schließen.</p>
+					<p className="text-xl w-3/5 text-center">Wir haben deinen Auftrag erhalten! Du kannst diese Website jetzt schließen.</p>
 				</div>
 			</div>
 		</>
